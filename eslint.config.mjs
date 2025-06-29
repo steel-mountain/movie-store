6573b568f1e851739427d1a392c29fc1b5d38f2c
steel-mountain <wazuup.lw@mail.ru>
2025-06-29 14:12:30 +0300

    update eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,23 @@
 import js from "@eslint/js"
-import pluginReact from "eslint-plugin-react"
-import { defineConfig } from "eslint/config"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import { globalIgnores } from "eslint/config"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+export default tseslint.config([
+  globalIgnores(["dist"]),
   {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/display-name": "off",
-      "react/prop-types": "off",
-      "react/no-unescaped-entities": "off",
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
   },
-  { ignores: ["build/**", "node_modules/**"] },
 ])
